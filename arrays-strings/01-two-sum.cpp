6921d26aecfaa2b37a392755594c34bd733cc145
Sivaranjan T <sivaranjansivs@gmail.com>
2026-09-16 14:02:47 +0530

    Implement two-sum function with test cases
    Implemented a two-sum function that returns indices of two numbers that add up to a target. Added test cases to validate the functionality.

--- arrays-strings/01-two-sum.cpp
+++ arrays-strings/01-two-sum.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+vector<int> twoSum(vector<int>& nums, int target)
+{
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                return {i, j};
+            }
+        }
+    }
+
+    return {};
+}
+
+int main()
+{
+    // Test Case 1
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+
+    vector<int> result1 = twoSum(nums1, target1);
+
+    cout << "Test Case 1: ";
+    cout << "[" << result1[0] << ", " << result1[1] << "]" << endl;
+
+
+    // Test Case 2 - Edge Case
+    vector<int> nums2 = {3, 3};
+    int target2 = 6;
+
+    vector<int> result2 = twoSum(nums2, target2);
+
+    cout << "Test Case 2: ";
+    cout << "[" << result2[0] << ", " << result2[1] << "]" << endl;
+
+    return 0;
+}
